171ae82c50cb8db264b7bf8af18334d36754d381
sleeter <lfmonsterfeed@gmail.com>
2024-10-25 00:23:36 +0300

    hw-3

--- hw_2/main.py
+++ hw_2/main.py
@@ -1,6 +1,7 @@
 from hw_2.controllers import cart, item
 import uvicorn
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Shop API")
 
@@ -12,6 +13,8 @@ def pong():
 
 app.include_router(cart.router)
 app.include_router(item.router)
+
+Instrumentator().instrument(app).expose(app)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
